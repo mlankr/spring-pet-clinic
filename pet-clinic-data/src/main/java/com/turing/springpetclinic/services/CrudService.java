dc7b4f5132baf67e933dbde9b85e4a4e3fb100f5
Milan <milan.koirala@stud.th-luebeck.de>
2023-02-16 21:51:38 +0100

    refactor common crud interface

---
+++ pet-clinic-data/src/main/java/com/turing/springpetclinic/services/CrudService.java
@@ -0,0 +1,20 @@
+package com.turing.springpetclinic.services;
+
+import java.util.Set;
+
+/**
+ * Created by Milan on 2023/02/16.
+ */
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteByID(ID id);
+}
--- pet-clinic-data/src/main/java/com/turing/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/turing/springpetclinic/services/VetService.java
@@ -2,16 +2,9 @@ package com.turing.springpetclinic.services;
 
 import com.turing.springpetclinic.model.Vet;
 
-import java.util.Set;
-
 /**
  * Created by Milan on 2023/02/14.
  */
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
