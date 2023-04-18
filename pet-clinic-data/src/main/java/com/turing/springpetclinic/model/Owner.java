fb7134c1939ce22c397e0a269e7a226a12e0dc61
Milan <milan.koirala@stud.th-luebeck.de>
2023-04-18 13:58:04 +0200

    updated owner and pet type service

--- pet-clinic-data/src/main/java/com/turing/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/turing/springpetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package com.turing.springpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
 /**
  * Created by Milan on 2023/02/14.
  */
-
+@Getter
+@Setter
 public class Owner extends Person {
+
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
 }
--- pet-clinic-data/src/main/java/com/turing/springpetclinic/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/com/turing/springpetclinic/services/PetTypeService.java
@@ -1,11 +1,10 @@
 package com.turing.springpetclinic.services;
 
 import com.turing.springpetclinic.model.PetType;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by Milan on 2023/04/18.
  */
-@Service
+
 public interface PetTypeService extends CrudService<PetType, Long> {
 }
