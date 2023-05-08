59c2ed0c989e08a6e6609051383a60f09a519741
Milan <milan.koirala@stud.th-luebeck.de>
2023-05-08 14:37:56 +0200

    updated base entity

--- pet-clinic-data/src/main/java/com/turing/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/turing/springpetclinic/model/BaseEntity.java
@@ -1,5 +1,9 @@
 package com.turing.springpetclinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +16,10 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
