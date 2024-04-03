03a53b841b4509714beb9ae2497add244ee6c4e8
Milan <koi.melon10@gmail.com>
2024-04-03 18:12:52 +0200

    add CORS setup

--- pet-clinic-web/src/main/java/com/turing/springpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/turing/springpetclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.turing.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+	@CrossOrigin(origins = "*")
 	@RequestMapping({"", "/", "/index", "/index.html"})
 	public String index() {
 		return "index";
